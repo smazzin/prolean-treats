875aec1c5f4509c7201f20ea18fa2d43281ae0e8
Josh Hausotter <jhausotter@gmail.com>
2021-03-23 16:01:42 -0700

    add npm scripts for themekit

---
+++ .dev/scripts/deploy.js
@@ -0,0 +1,10 @@
+import themeKit from '@shopify/themekit';
+import yargs from 'yargs';
+
+const { env = 'development', allowLive = false } = yargs(process.argv).argv;
+
+async function deploy() {
+	await themeKit.command('deploy', { env, allowLive });
+}
+
+deploy();
---
+++ .dev/scripts/watch.js
@@ -0,0 +1,11 @@
+import themeKit from '@shopify/themekit';
+import yargs from 'yargs';
+
+const { env = 'development', allowLive = true } = yargs(process.argv).argv;
+
+async function watch() {
+	await themeKit.command('deploy', { env, allowLive });
+	await themeKit.command('watch', { env, allowLive });
+}
+
+watch();
